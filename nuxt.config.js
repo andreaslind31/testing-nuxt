2fb8681e467914f865e048d300130f1dcd5dcc5e
Andreas Lind <Andreaslind31@gmail.com>
2021-12-29 23:00:15 +0100

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'testing-nuxt',
+    title: 'portfolio',
     htmlAttrs: {
       lang: 'en'
     },
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/faviconAL.ico' }
     ]
   },
 ssr: false,
